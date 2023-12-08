7a718eed96f096b503e9a04dee8ad1f83a9574a7
Alex Jensen <alex@endure.io>
2023-12-07 22:17:19 -0500

    part 1.1

--- spec/clj/y2023/day1_spec.clj
+++ spec/clj/y2023/day1_spec.clj
@@ -1,9 +1,30 @@
 (ns y2023.day1-spec
-  (:require [speclj.core :refer :all]))
+  (:require [speclj.core :refer :all]
+            [y2023.day1 :as sut]))
 
 (describe "day 1"
 
-  (context "parser"
+  (context "part 1"
+    (context "parser"
 
-    (it "finds value with numbers at beginning and end"
-      ())))
+      (context "for line"
+
+        (it "finds value with numbers at beginning and end"
+          (should= 12 (sut/parse-ln "1abc2")))
+
+        (it "finds value with numbers inside line"
+          (should= 38 (sut/parse-ln "pqr3stu8vwx")))
+
+        (it "finds value with more than 2 numbers"
+          (should= 15 (sut/parse-ln "a1b2c3d4e5f")))
+
+        (it "finds value with single number"
+          (should= 77 (sut/parse-ln "treb7uchet"))))
+
+      (context "for file"
+
+        (it "finds value for each line"
+          (should= [12 38 15 77] (sut/parse-file "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"))))))
+
+  (context "part 2"
+    ))
